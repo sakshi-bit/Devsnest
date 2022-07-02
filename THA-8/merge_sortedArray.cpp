56ee6f57053e48af728a0505d8cfde9d8f62e8c5
sakshi_bit <17june2001@gmail.com>
2022-07-03 05:11:13 +0530

    merged

---
+++ THA-8/merge_sortedArray.cpp
@@ -0,0 +1,17 @@
+vector<int> solve(int m, int n, vector<int> arr1, vector<int> arr2){
+    int i=m-1;
+    int j=n-1;
+    int k=m+n-1;
+    while(i>=0 && j>=0){
+        if(arr1[i]>=arr2[j]){
+            arr1[k--] = arr1[i--];
+        }else{
+            arr1[k--] = arr2[j--];
+        }
+    }
+
+    while(j>=0){
+        arr1[k--] = arr2[j--];
+    }
+    return arr1;
+}
