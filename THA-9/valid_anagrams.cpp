f82d6069ad78b6bb5a073dec08ad87fab3f6ae8c
sakshi <17june2001@gmail.com>
2022-07-06 13:24:36 +0530

    doodle

---
+++ THA-9/valid_anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+            return false;
+        int n = s.length();
+        int anagrams[26]={0};
+        for(int i=0;i<n;i++){
+            anagrams[s[i]-'a']++;
+            anagrams[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(anagrams[i])
+                return false;
+        }
+
+        return true;
+    }
+};
